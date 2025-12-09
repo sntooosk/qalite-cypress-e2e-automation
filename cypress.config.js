81b43e0f08f8b77809c848d146c1313af517b8ff
Juliano Cassimiro Dos Santos <Juliano.cassimiro@qualitydigital.global>
2025-12-09 15:45:49 -0300

    chore: restore cypress env json usage

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,5 @@
-require('dotenv').config()
 const { defineConfig } = require('cypress')
 const cucumber = require('cypress-cucumber-preprocessor').default
-
-const buildCredentials = () => ({
-  admin: {
-    email: process.env.ADMIN_EMAIL,
-    password: process.env.ADMIN_PASSWORD,
-  },
-  user: {
-    email: process.env.USER_EMAIL,
-    password: process.env.USER_PASSWORD,
-  },
-})
 
 module.exports = defineConfig({
   video: true,
@@ -21,7 +9,6 @@ module.exports = defineConfig({
   },
   defaultCommandTimeout: 120000,
   pageLoadTimeout: 180000,
-  env: buildCredentials(),
   e2e: {
     baseUrl: 'https://qa-lite.vercel.app',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx,feature}',
